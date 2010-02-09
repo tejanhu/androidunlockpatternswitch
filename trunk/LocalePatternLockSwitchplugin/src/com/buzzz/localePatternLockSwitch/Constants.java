ffa694b695e148a44ae2a6df94b913e00e48d3bb
Waatze <Waatze@5b069dc8-15b5-11df-9a75-2f9892f9ffe9>
2010-02-09 20:42:53 +0000

    Initial checkin

---
+++ trunk/LocalePatternLockSwitchplugin/src/com/buzzz/localePatternLockSwitch/Constants.java
@@ -0,0 +1,7 @@
+package com.buzzz.localePatternLockSwitch;
+
+public class Constants {
+
+	//constant for the extra key to store the setting
+	final static String LOCKSCREEN_SETTING = "com.buzzzsoft.localePatternLockSwitch.SWITCHSETTING";
+}
